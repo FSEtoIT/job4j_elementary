dfba5f65c5543eeaee1b5f0482b57dc4625eba34
FSE <amakeevmindraycom@yandex.ru>
2025-07-11 17:45:42 +0300

    1. Модульные тесты [#505277]_2

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -7,18 +7,18 @@ class ConverterTest {
 
     @Test
     void whenConvert140RblThen2Euro() {
-        float input = 140;
-        float expected = 2;
-        float output = (float) Converter.rubleToEuro(input);
+        float input = 140f;
+        float expected = 1.5556f;
+        float output = Converter.rubleToEuro(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
 
     @Test
-    void whenConvert180RblThen3Dollar() {
+    void whenConvert200RblThen3dot3333Euro() {
         float input = 180;
-        float expected = 3;
-        float output = (float) Converter.rubleToDollar(input);
+        float expected = 2.4000f;
+        float output = Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
