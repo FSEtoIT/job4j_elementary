8eef92afd61e2dc105a90dd00e8aab641b8c4efe
FSE <amakeevmindraycom@yandex.ru>
2025-07-11 19:22:05 +0300

    1.1. Что такое import [#505278]_5

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -3,24 +3,24 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
-    public static double sumAndMultiply(double first, double second) {
+        public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                + multiply(first, second);
     }
 
-    public static double decAndDiv(double first, double second) {
+        public static double decAndDiv(double first, double second) {
         return dec(first, second)
                 + div(first, second);
     }
 
-    public static double sumAndMultiplyAndDecAndDiv(double first, double second) {
+        public static double sumAndMultiplyAndDecAndDiv(double first, double second) {
         return sum(first, second)
                 + multiply(first, second)
                     + dec(first, second)
                         + div(first, second);
     }
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Результат расчета СУМА+УМНОЖЕНИЕ равен: " + sumAndMultiply(10, 20));
         System.out.println("Результат расчета РАЗНИЦА+ДЕЛЕНИЕ равен: " + decAndDiv(10, 20));
         System.out.println("Результат расчета СУМА+УМНОЖЕНИЕ+РАЗНИЦА+ДЕЛЕНИЕ равен: " + sumAndMultiplyAndDecAndDiv(10, 20));
