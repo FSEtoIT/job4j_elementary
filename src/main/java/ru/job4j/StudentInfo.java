5e217569b1ba9e507d562ffec9a6b75b92c3ce79
FSE <amakeevmindraycom@yandex.ru>
2025-07-11 13:52:32 +0300

    README to git [#3]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrey FSE");
+        System.out.println("23.07.1988");
+    }
+}
