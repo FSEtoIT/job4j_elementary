c70545b9866954a4fb403746f645c67b5bf12a32
FSE <amakeevmindraycom@yandex.ru>
2025-07-21 07:08:27 +0300

    2. Тесты для задачи "идеальный вес" [#505260]_w/o comments

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,12 +2,12 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    public static float rubleToEuro(float value) { // поменял на float, чтобы учитывать 4 знака после занятой
+    public static float rubleToEuro(float value) {
         // Assuming the conversion rate is 1 Euro = 90 Rubles
         return value / 90;
     }
 
-    public static float rubleToDollar(float value) {  // поменял на float, чтобы учитывать 4 знака после занятой
+    public static float rubleToDollar(float value) {
         // Assuming the conversion rate is 1 Dollar = 75 Rubles
         return value / 75;
     }
--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -8,7 +8,7 @@ class ConverterTest {
     @Test
     void whenConvert140RblThen2Euro() {
         float input = 140f;
-        float expected = 1.5556f; // 4 знака после запятой
+        float expected = 1.5556f;
         float output = Converter.rubleToEuro(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
@@ -17,7 +17,7 @@ class ConverterTest {
     @Test
     void whenConvert200RblThen3dot3333Euro() {
         float input = 180;
-        float expected = 2.4000f; // 4 знака после запятой
+        float expected = 2.4000f;
         float output = Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
