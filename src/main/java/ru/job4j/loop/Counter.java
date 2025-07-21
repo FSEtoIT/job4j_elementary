bffbd73ebb0ca7cd5ff084c0c4697e6a92c23792
FSE <amakeevmindraycom@yandex.ru>
2025-07-21 13:53:18 +0300

    Подсчет суммы чётных чисел в диапазоне [#505210]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,17 +3,21 @@ package ru.job4j.loop;
 public class Counter {
 
     public static int sum(int start, int finish) {
-        int sum = 0;
+        int sum;
         for (sum = 0; sum <= 10; sum++) {
             sum = sum + sum++;
         }
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-        System.out.println(sum(3, 1));
+    public static int sumByEven(int start, int finish) {
+        int sum;
+        int sumEven = 0;
+        for (sum = 0; sum <= 10; sum++) {
+            if ((sum % 2) == 0) {
+                sumEven = sumEven + sum++;
+            }
+            }
+        return sumEven;
     }
 }
--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -3,6 +3,7 @@ package ru.job4j.loop;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import static ru.job4j.loop.Counter.sum;
+import static ru.job4j.loop.Counter.sumByEven;
 
 class CounterTest {
 
@@ -35,6 +36,54 @@ class CounterTest {
         int sum = 0;
         int expected = 0;
         int output = sum(3, 1);
+        assertThat(output).isNotEqualTo(expected);
+    }
+
+    @Test
+    void whenStartMinus3Finish1ThenSum15() {
+        int sum = 0;
+        int expected = 0;
+        int output = sum(-3, 1);
+        assertThat(output).isNotEqualTo(expected);
+    }
+
+    @Test
+    void whenStart0Finish10ThenSumByEven30() {
+        int sumEven = 0;
+        int expected = 30;
+        int output = sumByEven(0, 10);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart3Finish8ThenSumByEven30() {
+        int sumEven = 0;
+        int expected = 30;
+        int output = sumByEven(3, 8);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart1Finish1ThenSumByEven30() {
+        int sumEven = 0;
+        int expected = 30;
+        int output = sumByEven(1, 1);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart3Finish1ThenSumByEven30() {
+        int sumEven = 0;
+        int expected = 30;
+        int output = sumByEven(3, 1);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStartMinus3Finish1ThenSumByEven30() {
+        int sumEven = 0;
+        int expected = 0;
+        int output = sumByEven(-3, 1);
         assertThat(output).isNotEqualTo(expected);
     }
 }
