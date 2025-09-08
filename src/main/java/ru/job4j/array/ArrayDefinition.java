0ec0d79dfa1013eb0e3157fd52aa4e8d4471f5ea
FSE <amakeevmindraycom@yandex.ru>
2025-09-08 16:30:23 +0300

    Размер массива [#505221]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,9 +1,12 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    public static void main() {
+    public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер массива short равен: " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("Размер массива String равен: " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Размер массива float равен: " + prices.length);
     }
 }
