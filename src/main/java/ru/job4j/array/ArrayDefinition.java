419c99e8d87ce99c57d535784df2064247c905c2
FSE <amakeevmindraycom@yandex.ru>
2025-09-08 16:42:55 +0300

    Заполнение массива [#505222]_2

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,10 +3,21 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Размер массива short равен: " + ages.length);
+        System.out.println("Размер массива ages равен: " + ages.length);
+        String[] names = new String[4];
+        System.out.println("Размер массива names равен: " + names.length);
         String[] surnames = new String[100500];
-        System.out.println("Размер массива String равен: " + surnames.length);
+        System.out.println("Размер массива surnames равен: " + surnames.length);
         float[] prices = new float[40];
-        System.out.println("Размер массива float равен: " + prices.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
+        names[0] = "Andrey";
+        names[1] = "Nastya";
+        names[2] = "Dima";
+        names[3] = "Avi";
+        System.out.println("========Current Names in DB: ========");
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
