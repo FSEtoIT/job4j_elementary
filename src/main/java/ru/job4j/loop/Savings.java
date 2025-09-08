890fca18b9f8bcc62e03d28197706deedbb891a5
FSE <amakeevmindraycom@yandex.ru>
2025-09-08 16:08:20 +0300

    Цикл do-while [#505216]

---
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Savings {
+
+    public static int years(double goal, double annualDeposit, double percent) {
+        int years = 0;
+        double total = 0;
+        do {
+            if (years > 1) {
+                total += total * percent / 100;
+            }
+            years++;
+            total += annualDeposit;
+        } while (goal >= total);
+        return years;
+    }
+}
